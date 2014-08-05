97d5dc7f246b0fd76f41b898a5c15e840c5d330d
ritmatter <mritter123@gmail.com>
2014-08-04 23:49:11 -0700

    Add some error handling for text input

--- app/routes/twilio_router.js
+++ app/routes/twilio_router.js
@@ -1,4 +1,5 @@
 var Point = require('../models/Point.js');
+var User = require('../models/User.js');
 var twilio = require('twilio');
 var client = new twilio.RestClient();
 var resp = new twilio.TwimlResponse();
@@ -14,42 +15,59 @@ module.exports = function(router) {
             var message = req.body.Body;
             var from = req.body.From;
 
-            var type = message.split(" ")[0];
-            var data = message.split(" ")[1];
-            var unit = message.split(" ")[2];
-            var userId = User.find({
-                phoneNumber : from,
-            }, function(err, user) {
-                if (err)
-                    res.send(err);
-            });
+            var input = message.split(" ");
+            if (input.length != 3 || isNaN(input[1])) {
+                response = "Sorry, we were unable to process that. Please make sure you enter <VERB> <MEASUREMENT> <UNIT>";
+                sendText(from, message, response);
+            } else {
+                var type = input[0];
+                var data = input[1];
+                var unit = input[2];
 
-            new Point({
-                type          : type,
-                time_stamp    : Date.now(),
-                user_id       : '1993',
-                unit          : unit,
-                data          : data
-            }).save( function(err) {
-                if (err)
-                    res.send(err);
-                res.json({ message : 'Point created' });
-            });
-
-            client.sms.messages.post({
-                to: from,
-                from: TWILIO_NUMBER,
-                body: 'Received message: ' + message
-            }, function(err, message) {
-                if (!err) {
-                    console.log('Success! The SID for this SMS message is:');
-                    console.log(message.sid);
-                    console.log('Message sent on:');
-                    console.log(message.dateCreated);
-                } else {
-                    console.log(error);
-                    console.log('Oops! There was an error.');
-                }
-            });
-        });
+                User.findOne({
+                    phoneNumber : from,
+                }, function(err, user) {
+                    if (err) {
+                        response = "Sorry, something went wrong. Please send your text again";
+                        sendText(from, message, response);
+                    }
+                    else if (!user) {
+                        response = "Hi there! We don't recognize your number. If you'd like to sign up, text us \"u:<USERNAME>, p:<PASSWORD>\"";
+                        sendText(from, message, response);
+                    } else {
+                        new Point({
+                            type          : type,
+                            time_stamp    : Date.now(),
+                            user_id       : user.userId,
+                            unit          : unit,
+                            data          : data
+                        }).save( function(err) {
+                            if (err)
+                                res.send(err);
+                            res.json({ message : 'Point created' });
+                        });
+                        response = "Successfully processed " + "\"" + message + "\"";
+                        sendText(from, message, response);
+                    }
+                });
+            }
+       });
 };
+
+function sendText(from, message, response) {
+    client.sms.messages.post({
+        to: from,
+        from: TWILIO_NUMBER,
+        body: response
+    }, function(err, message) {
+        if (!err) {
+            console.log('Success! The SID for this SMS message is:');
+            console.log(message.sid);
+            console.log('Message sent on:');
+            console.log(message.dateCreated);
+        } else {
+            console.log(error);
+            console.log('Oops! There was an error.');
+        }
+    });
+}
