f3efe91c5014d832fa72849a6f75d6f493d64adc
Stephen Malina <stephen.malina@urbancompass.com>
2014-08-08 18:42:44 -0400

    Working test for VisualizationController

--- public/test/controller.js
+++ public/test/controller.js
@@ -1,8 +1,35 @@
-describe('MainController', function() {
+describe('Initialize Controller', function() {
   var scope, controller, mockPointsService = {};
-  beforeEach(angular.mock.module('remembrallApp.controllers'));
-  it('should have a VisualizationController', inject(function($controller) {
-    var VisualizationController = $controller('VisualizationController', { $scope: {} });
-    expect(VisualizationController).toBeDefined();
-  }));
+  beforeEach(function () {
+    angular.mock.module('remembrallApp.controllers');
+    inject(function($rootScope, $controller, $q, _$timeout_) {
+      mockPoints =  {
+        getFriends: function () {}
+      }
+      spyOn(mockPoints, 'getFriends').andCallFake(function () {
+        deferred = $q.defer();
+        return {$promise: deferred.promise}
+      });
+      $scope = $rootScope.$new();
+      $rootScope.$apply();
+      $timeout = _$timeout_;
+      ctrl = $controller('VisualizationController', {
+        $scope: $scope,
+        Points: mockPoints
+      });
+    });
+  }); 
+  describe('VisualizationController specification', function () {
+    beforeEach(function () {
+      deferred.resolve([{"timeStamp": 100}]);
+      $scope.$apply();
+        
+    })
+    it('should format the service data\'s date', inject(function($controller, $rootScope) {
+      expect(mockPoints.getFriends).toHaveBeenCalled();
+      expect($scope.data).toEqual(
+        [{"timeStamp": 100, "dateTimeStamp": new Date(100)}]
+      );
+    }));
+  });
 });
